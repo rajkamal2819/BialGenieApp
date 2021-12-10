2cadc60798bdd55d2ec6552a478170c0aeb1f7fd
Rajkamal <rajkamal.singh1011@gmail.com>
2021-12-10 23:00:07 +0530

    commit: Created Adapter and Fragments Package, with LogIn and SignUp Fragments and Authentication section Front End work done.

--- app/src/main/java/com/Hackathon/bialgenieapp/Authentication.java
+++ app/src/main/java/com/Hackathon/bialgenieapp/Authentication.java
@@ -1,14 +1,45 @@
 package com.Hackathon.bialgenieapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.Hackathon.bialgenieapp.Adapters.FragmentAuthAdapter;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.tabs.TabLayout;
 
 public class Authentication extends AppCompatActivity {
+
+
+    FloatingActionButton googleBtn;
+    private static final String TAG = Authentication.class.getSimpleName();
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
+
+        viewPager = findViewById(R.id.viewpagerAuth);
+        tabLayout = findViewById(R.id.tabLayout);
+
+        viewPager.setAdapter(new FragmentAuthAdapter(getSupportFragmentManager()));
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        googleBtn = findViewById(R.id.google_auth_button);
+
+        googleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
+
+
     }
 }
