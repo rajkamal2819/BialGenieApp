e0ae95973f9363cda163dd39bb2ae2e939f3332d
Anirudh <singhaniruddh30@gmail.com>
2021-12-13 08:22:08 +0530

    Cab_ola and uber added

---
+++ app/src/main/java/com/Hackathon/bialgenieapp/Fragments/Transit/Transit_Cab_Ola.java
@@ -0,0 +1,66 @@
+package com.Hackathon.bialgenieapp.Fragments.Transit;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.Hackathon.bialgenieapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Transit_Cab_Ola#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Transit_Cab_Ola extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public Transit_Cab_Ola() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Transit_Cab_Ola.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Transit_Cab_Ola newInstance(String param1, String param2) {
+        Transit_Cab_Ola fragment = new Transit_Cab_Ola();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_transit__cab__ola, container, false);
+    }
+}
