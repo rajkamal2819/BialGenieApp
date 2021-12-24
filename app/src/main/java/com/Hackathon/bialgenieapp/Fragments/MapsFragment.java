f4e3a4f845dba6731f71b5c54183b18d051dc5f0
Rajkamal <rajkamal.singh1011@gmail.com>
2021-12-24 18:21:05 +0530

    commit: Shopping Malls , Cafe , Data source creation and deployment in the map with the Symbol layer.

--- app/src/main/java/com/Hackathon/bialgenieapp/Fragments/MapsFragment.java
+++ app/src/main/java/com/Hackathon/bialgenieapp/Fragments/MapsFragment.java
@@ -1,20 +1,34 @@
 package com.Hackathon.bialgenieapp.Fragments;
 
+import static com.azure.android.maps.control.options.LineLayerOptions.strokeColor;
+import static com.azure.android.maps.control.options.LineLayerOptions.strokeWidth;
 import static com.azure.android.maps.control.options.SymbolLayerOptions.iconImage;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.provider.Settings;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.Hackathon.bialgenieapp.MainActivity;
 import com.Hackathon.bialgenieapp.databinding.FragmentMapsBinding;
 import com.azure.android.maps.control.AzureMaps;
 import com.azure.android.maps.control.MapControl;
+import com.azure.android.maps.control.layer.LineLayer;
 import com.azure.android.maps.control.layer.SymbolLayer;
 import com.azure.android.maps.control.options.CameraOptions;
 import com.azure.android.maps.control.options.Expression;
@@ -23,9 +37,12 @@ import com.azure.android.maps.control.options.StyleOptions;
 import com.azure.android.maps.control.options.SymbolLayerOptions;
 import com.azure.android.maps.control.source.DataSource;
 import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.LineString;
 import com.mapbox.geojson.Point;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MapsFragment extends Fragment {
 
@@ -46,6 +63,10 @@ public class MapsFragment extends Fragment {
     FragmentMapsBinding binding;
     MapControl mapControl;
     int count = 0;
+    LocationManager locationManager;
+    String latitude,longitude;
+    private static  final int REQUEST_LOCATION=1;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -158,7 +179,7 @@ public class MapsFragment extends Fragment {
             Feature feature11 = Feature.fromGeometry(Point.fromLngLat(77.7063947,13.199459,1031));
             source11.add(feature11);
             SymbolLayer layer11 = new SymbolLayer(source11,SymbolLayerOptions.textField("Tiffin Center"));
-            layerList.add(layer10);
+            layerList.add(layer11);
 
             DataSource source12 = new DataSource();
             map.sources.add(source12);
@@ -239,15 +260,90 @@ public class MapsFragment extends Fragment {
             map.sources.add(source21);
             Feature feature21 = Feature.fromGeometry(Point.fromLngLat(77.708817,13.2002175,248));
             source21.add(feature21);
-            SymbolLayer layer21 = new SymbolLayer(source21,SymbolLayerOptions.textField("Swarovski"));
+            SymbolLayer layer21 = new SymbolLayer(source21,iconImage("marker-blue"),SymbolLayerOptions.textField("Swarovski"));
             layerList.add(layer21);
 
             DataSource source22 = new DataSource();
             map.sources.add(source22);
-            Feature feature22 = Feature.fromGeometry(Point.fromLngLat(77.708817,13.2002175,248));
+            Feature feature22 = Feature.fromGeometry(Point.fromLngLat(77.7083217,13.1993354,517));
             source22.add(feature22);
-            SymbolLayer layer22 = new SymbolLayer(source22,SymbolLayerOptions.textField("Swarovski"));
+            SymbolLayer layer22 = new SymbolLayer(source22,iconImage("marker-blue"),SymbolLayerOptions.textField("Duty Free"));
             layerList.add(layer22);
+
+            DataSource source23 = new DataSource();
+            map.sources.add(source23);
+            Feature feature23 = Feature.fromGeometry(Point.fromLngLat(77.708449,13.1995019,447));
+            source23.add(feature23);
+            SymbolLayer layer23 = new SymbolLayer(source23,iconImage("marker-blue"),SymbolLayerOptions.textField("FSTR"));
+            layerList.add(layer23);
+
+            DataSource source24 = new DataSource();
+            map.sources.add(source24);
+            Feature feature24 = Feature.fromGeometry(Point.fromLngLat(77.7085024,13.2000034,266));
+            source24.add(feature24);
+            SymbolLayer layer24 = new SymbolLayer(source24,iconImage("marker-blue"),SymbolLayerOptions.textField("Fossil and skagen store at airport"));
+            layerList.add(layer24);
+
+            DataSource source25 = new DataSource();
+            map.sources.add(source25);
+            Feature feature25 = Feature.fromGeometry(Point.fromLngLat(77.7082659,13.1998623,328));
+            source25.add(feature25);
+            SymbolLayer layer25 = new SymbolLayer(source25,iconImage("marker-blue"),SymbolLayerOptions.textField("Hidesign"));
+            layerList.add(layer25);
+
+            DataSource source26 = new DataSource();
+            map.sources.add(source26);
+            Feature feature26 = Feature.fromGeometry(Point.fromLngLat(77.7082885,13.199763,360));
+            source26.add(feature26);
+            SymbolLayer layer26 = new SymbolLayer(source26,iconImage("marker-blue"),SymbolLayerOptions.textField("Mithastop"));
+            layerList.add(layer26);
+
+            DataSource source27 = new DataSource();
+            map.sources.add(source27);
+            Feature feature27 = Feature.fromGeometry(Point.fromLngLat(77.7081766,13.199696,451));
+            source27.add(feature27);
+            SymbolLayer layer27 = new SymbolLayer(source27,iconImage("marker-blue"),SymbolLayerOptions.textField("Relay"));
+            layerList.add(layer27);
+
+            DataSource source28 = new DataSource();
+            map.sources.add(source28);
+            Feature feature28 = Feature.fromGeometry(Point.fromLngLat(77.7097926,13.2001642,414));
+            source28.add(feature28);
+            SymbolLayer layer28 = new SymbolLayer(source28,iconImage("marker-blue"),SymbolLayerOptions.textField("WHSmith"));
+            layerList.add(layer28);
+
+            DataSource source29 = new DataSource();
+            map.sources.add(source29);
+            Feature feature29 = Feature.fromGeometry(Point.fromLngLat(77.7077882,13.1993455,491));
+            source29.add(feature29);
+            SymbolLayer layer29 = new SymbolLayer(source29,iconImage("marker-blue"),SymbolLayerOptions.textField("Buzz"));
+            layerList.add(layer29);
+
+            DataSource source30 = new DataSource();
+            map.sources.add(source30);
+            Feature feature30 = Feature.fromGeometry(Point.fromLngLat(77.7081292,13.2001311,360));
+            source30.add(feature30);
+            SymbolLayer layer30 = new SymbolLayer(source30,iconImage("marker-blue"),SymbolLayerOptions.textField("The Lotus House"));
+            layerList.add(layer30);
+
+            // SHOPS LAYERS 21 to 30
+
+            /** LOUNGES **/
+
+            DataSource source31 = new DataSource();
+            map.sources.add(source31);
+            Feature feature31 = Feature.fromGeometry(Point.fromLngLat(77.6791899,13.1878196,4840));
+            source31.add(feature31);
+            SymbolLayer layer31 = new SymbolLayer(source31,iconImage("marker-black"),SymbolLayerOptions.textField("Taj Bangalore"));
+            layerList.add(layer31);
+
+            DataSource source32 = new DataSource();
+            map.sources.add(source32);
+            Feature feature32 = Feature.fromGeometry(Point.fromLngLat(77.6340573,13.1924542,4848));
+            source32.add(feature32);
+            SymbolLayer layer32 = new SymbolLayer(source32,iconImage("marker-black"),SymbolLayerOptions.textField("Regenta Inn Bangalore"));
+            layerList.add(layer32);
+
 
 
             binding.restaurants.setOnClickListener(new View.OnClickListener() {
@@ -255,6 +351,7 @@ public class MapsFragment extends Fragment {
                 public void onClick(View v) {
 
                   map.layers.remove(layerList.toArray(new SymbolLayer[layerList.size()]));
+                  map.setCamera(CameraOptions.zoom(17));
 
                     map.layers.add(layer6);
                     map.layers.add(layer7);
@@ -274,6 +371,7 @@ public class MapsFragment extends Fragment {
                 public void onClick(View v) {
 
                     map.layers.remove(layerList.toArray(new SymbolLayer[layerList.size()]));
+                    map.setCamera(CameraOptions.zoom(16));
 
                     map.layers.add(layer0);
                     map.layers.add(layer1);
@@ -290,6 +388,7 @@ public class MapsFragment extends Fragment {
                 public void onClick(View v) {
 
                     map.layers.remove(layerList.toArray(new SymbolLayer[layerList.size()]));
+                    map.setCamera(CameraOptions.zoom(16));
 
                     map.layers.add(layer15);
                     map.layers.add(layer16);
@@ -304,9 +403,44 @@ public class MapsFragment extends Fragment {
                 public void onClick(View v) {
 
                     map.layers.remove(layerList.toArray(new SymbolLayer[layerList.size()]));
+                    map.setCamera(CameraOptions.zoom(16));
 
                     map.layers.add(layer19);
                     map.layers.add(layer20);
+
+                }
+            });
+
+            binding.shopping.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    map.layers.remove(layerList.toArray(new SymbolLayer[layerList.size()]));
+                    map.setCamera(CameraOptions.zoom(17));
+
+                    map.layers.add(layer21);
+                    map.layers.add(layer22);
+                    map.layers.add(layer23);
+                    map.layers.add(layer24);
+                    map.layers.add(layer25);
+                    map.layers.add(layer26);
+                    map.layers.add(layer27);
+                    map.layers.add(layer28);
+                    map.layers.add(layer29);
+                    map.layers.add(layer30);
+
+                }
+            });
+
+            binding.lounges.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    map.layers.remove(layerList.toArray(new SymbolLayer[layerList.size()]));
+                    map.setCamera(CameraOptions.zoom(12));
+
+                    map.layers.add(layer31);
+                    map.layers.add(layer32);
 
                 }
             });
@@ -375,4 +509,5 @@ public class MapsFragment extends Fragment {
         super.onSaveInstanceState(outState);
         mapControl.onSaveInstanceState(outState);
     }
+
 }
